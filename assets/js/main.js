c2f94f2a46b5abe2e512a82248a095af4effd7bc
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-17 17:23:54 +0200

    modifica script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,4 @@
-/* l programma dovrà chiedere all’utente il numero di chilometri
-e l’età
-e dovrà calcolare il prezzo totale del viaggio.
-Il prezzo del biglietto è definito in base ai km (0.21 €cent al km)
-ma c’è uno sconto del 20% per i minorenni
-e del 40% per gli over 65
-NOTE:
-- parto dal rileggere e comprendere a fondo la richiesta;
-- per sicurezza me la copio come commento nel mio js;
-- parto di commenti e/o pseudo code;
-- inizio a scrivere passo passo il codice reale della mia app;
-- quindi ogni tot faccio dei log per vedere se tutto mi torna e poi vado avanti;
-- parto con dei semplici log e se E SOLO SE tutto funziona e ho capito quello che ho fatto, vado a migliorare il mio output in pagina con html+css.*/
+// js calcolo tariffa tratta
 
 // general
 var kmReq;
